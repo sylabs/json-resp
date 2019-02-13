ec5a951904270ed3681e71bd657fe27b4120a2d7
Adam Hughes <stickmanica@gmail.com>
2019-02-13 09:43:06 -0500

    Add tests for WriteResponsePage and ReadResponsePage

--- json_response_test.go
+++ json_response_test.go
@@ -79,6 +79,59 @@ func TestWriteError(t *testing.T) {
 	}
 }
 
+func TestWriteResponsePage(t *testing.T) {
+	type TestStruct struct {
+		Value string
+	}
+
+	tests := []struct {
+		name      string
+		data      interface{}
+		pd        *PageDetails
+		code      int
+		wantValue string
+		wantPD    *PageDetails
+		wantCode  int
+	}{
+		{"Empty", TestStruct{""}, nil, http.StatusOK, "", nil, http.StatusOK},
+		{"NotEmpty", TestStruct{"blah"}, nil, http.StatusOK, "blah", nil, http.StatusOK},
+		{"PageNone", TestStruct{"blah"}, &PageDetails{}, http.StatusOK, "blah", &PageDetails{}, http.StatusOK},
+		{"PagePrev", TestStruct{"blah"}, &PageDetails{Prev: "p"}, http.StatusOK, "blah", &PageDetails{Prev: "p"}, http.StatusOK},
+		{"PageNext", TestStruct{"blah"}, &PageDetails{Next: "n"}, http.StatusOK, "blah", &PageDetails{Next: "n"}, http.StatusOK},
+		{"PagePrevNext", TestStruct{"blah"}, &PageDetails{Prev: "p", Next: "n"}, http.StatusOK, "blah", &PageDetails{Prev: "p", Next: "n"}, http.StatusOK},
+		{"Created", TestStruct{"blah"}, nil, http.StatusCreated, "blah", nil, http.StatusCreated},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+
+			WriteResponsePage(rr, tt.data, tt.pd, tt.code)
+
+			var ts TestStruct
+			pd, err := ReadResponsePage(rr.Body, &ts)
+			if err != nil {
+				t.Fatalf("failed to decode response: %v", err)
+			}
+			if ts.Value != tt.wantValue {
+				t.Errorf("got value '%v', want '%v'", ts.Value, tt.wantValue)
+			}
+			if got, want := (pd == nil), (tt.wantPD == nil); got != want {
+				t.Errorf("got nil page %v, want %v", got, want)
+			} else if pd != nil {
+				if got, want := pd.Prev, tt.wantPD.Prev; got != want {
+					t.Errorf("got prev %v, want %v", got, want)
+				}
+				if got, want := pd.Next, tt.wantPD.Next; got != want {
+					t.Errorf("got next %v, want %v", got, want)
+				}
+			}
+			if rr.Code != tt.wantCode {
+				t.Errorf("got code '%v', want '%v'", rr.Code, tt.wantCode)
+			}
+		})
+	}
+}
+
 func TestWriteResponse(t *testing.T) {
 	type TestStruct struct {
 		Value string
@@ -115,16 +168,68 @@ func TestWriteResponse(t *testing.T) {
 	}
 }
 
-func getResponseBody(v interface{}) io.Reader {
+func getResponseBodyPage(v interface{}, p *PageDetails) io.Reader {
 	rr := httptest.NewRecorder()
-	WriteResponse(rr, v, http.StatusOK)
+	WriteResponsePage(rr, v, p, http.StatusOK)
 	return rr.Body
+}
+
+func getResponseBody(v interface{}) io.Reader {
+	return getResponseBodyPage(v, nil)
 }
 
 func getErrorBody(error string, code int) io.Reader {
 	rr := httptest.NewRecorder()
 	WriteError(rr, error, code)
 	return rr.Body
+}
+
+func TestReadResponsePage(t *testing.T) {
+	type TestStruct struct {
+		Value string
+	}
+
+	tests := []struct {
+		name      string
+		r         io.Reader
+		wantErr   bool
+		wantValue string
+		wantPD    *PageDetails
+	}{
+		{"Empty", bytes.NewReader(nil), true, "", nil},
+		{"Response", getResponseBody(TestStruct{"blah"}), false, "blah", nil},
+		{"ResponsePageNone", getResponseBodyPage(TestStruct{"blah"}, &PageDetails{}), false, "blah", &PageDetails{}},
+		{"ResponsePagePrev", getResponseBodyPage(TestStruct{"blah"}, &PageDetails{"prev", ""}), false, "blah", &PageDetails{Prev: "prev"}},
+		{"ResponsePageNext", getResponseBodyPage(TestStruct{"blah"}, &PageDetails{"", "next"}), false, "blah", &PageDetails{Next: "next"}},
+		{"ResponsePagePrevNext", getResponseBodyPage(TestStruct{"blah"}, &PageDetails{"prev", "next"}), false, "blah", &PageDetails{Prev: "prev", Next: "next"}},
+		{"Error", getErrorBody("blah", http.StatusNotFound), true, "", nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var ts TestStruct
+
+			pd, err := ReadResponsePage(tt.r, &ts)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadResponse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if err == nil {
+				if ts.Value != tt.wantValue {
+					t.Errorf("got value '%v', want '%v'", ts.Value, tt.wantValue)
+				}
+				if got, want := (pd == nil), (tt.wantPD == nil); got != want {
+					t.Errorf("got nil page %v, want %v", got, want)
+				} else if pd != nil {
+					if got, want := pd.Prev, tt.wantPD.Prev; got != want {
+						t.Errorf("got prev %v, want %v", got, want)
+					}
+					if got, want := pd.Next, tt.wantPD.Next; got != want {
+						t.Errorf("got next %v, want %v", got, want)
+					}
+				}
+			}
+		})
+	}
 }
 
 func TestReadResponse(t *testing.T) {
